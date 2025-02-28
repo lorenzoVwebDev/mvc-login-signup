68f9e35bd19c8ae14167aaac485dc36bd25a715d
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2025-02-28 21:30:01 +0100

    updated

--- public/assets/js/main/home.js
+++ public/assets/js/main/home.js
@@ -2,19 +2,19 @@ import {url} from '../utils/globalVariables.js'
 //services
 import { fetchDashboard } from '../services/home/fetchPages.js';
 //view
-import { renderDashboard } from '../view/dashboard/'
+import { renderDashboard } from '../view/dashboard/dashboard.view.js'
 
-window.addEventListener('popstate', () => {
-  console.log("URL changed to:", window.location.pathname);
-});
+
 
 window.addEventListener('load', () => {
 
-
+  window.addEventListener('popstate', () => {
+    console.log("URL changed to:", window.location.pathname);
+  });
   document.getElementById('dashboard-link').addEventListener('click', async () => {
     const responseObject = await fetchDashboard(url);
     const {result, response } = responseObject;
 
-    
+
   })
 })
