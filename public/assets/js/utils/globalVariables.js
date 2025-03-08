1037715ce03b1a78888385e847921997be357a0a
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2025-03-08 20:54:03 +0100

    updated

--- public/assets/js/utils/globalVariables.js
+++ public/assets/js/utils/globalVariables.js
@@ -1,5 +1,5 @@
 const server = 'https://apachebackend.lorenzo-viganego.com/mvc-login-signup/public/';
 const local = 'http://mvc-login-signup/public/'
-export const url = local;
+export const url = server;
 //access token
 export let accessToken = '';
